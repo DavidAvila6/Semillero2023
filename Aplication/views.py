bf8a98b8e87d08020e35d0dbd21e7c64f2e0a323
Daniela Pinzon <danispc389@gmail.com>
2023-11-09 11:25:06 -0500

    template login sin conexion

--- Aplication/views.py
+++ Aplication/views.py
@@ -1,9 +1,12 @@
 from django.http import JsonResponse
+from django.shortcuts import render,HttpResponse,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
 from django.shortcuts import render
 from .models import Historial, Movimiento, Producto, Usuario
 from .forms import DevolucionProductoForm, UsuarioForm,ProductoForm,EntregaProductoForm,Movimiento
-
 
 def usuarios(request):
     return render(request, 'usuarios.html')
@@ -162,3 +165,43 @@ def historial(request):
 def informacion_historial(request, historial_id):
     historial = get_object_or_404(Historial, pk=historial_id)  # Asegúrate de importar el modelo Historial
     return render(request, 'informacion_historial.html', {'historial': historial})
+
+def HomePage(request):
+    return render (request,'home.html')
+
+def SignupPage(request):
+    if request.method=='POST':
+        uname=request.POST.get('username')
+        email=request.POST.get('email')
+        pass1=request.POST.get('password1')
+        pass2=request.POST.get('password2')
+
+        if pass1!=pass2:
+            return HttpResponse("Your password and confrom password are not Same!!")
+        else:
+
+            my_user=User.objects.create_user(uname,email,pass1)
+            my_user.save()
+            return redirect('login')
+        
+
+
+
+    return render (request,'signup.html')
+
+def LoginPage(request):
+    if request.method=='POST':
+        username=request.POST.get('username')
+        pass1=request.POST.get('pass')
+        user=authenticate(request,username=username,password=pass1)
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            return HttpResponse ("Username or Password is incorrect!!!")
+
+    return render (request,'login.html')
+
+def LogoutPage(request):
+    logout(request)
+    return redirect('login')
--- Semillero/urls.py
+++ Semillero/urls.py
@@ -15,12 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from Aplication import views
+from django.contrib.auth import views as auth_views
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.principal),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('principal/', views.principal, name='principal'),
     path('agregar-usuario/', views.agregar_usuario, name='agregar_usuario'),
     path('listar-usuarios/', views.listar_usuarios, name='listar_usuarios'),
