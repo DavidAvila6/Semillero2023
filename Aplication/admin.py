c5195c666438fb42d71b4fb89215310178df46ac
DavidAvila6 <david.avila02sa.edu.co>
2023-11-29 16:09:41 -0500

    PRODUCTOS

--- Aplication/admin.py
+++ Aplication/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Usuario,Producto,Movimiento,DevolucionProducto,Historial
+from .models import Usuario,Producto,Movimiento,DevolucionProducto,Historial,Inventario
 
 admin.site.register(Usuario)
 admin.site.register(Producto)
 admin.site.register(Movimiento)
 admin.site.register(DevolucionProducto)
 admin.site.register(Historial)
+admin.site.register(Inventario)
--- Aplication/models.py
+++ Aplication/models.py
@@ -10,15 +10,19 @@ class Usuario(models.Model):
     def __str__(self):
         return self.nombre
     
+# en models.py de tu aplicación
 class Producto(models.Model):
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=255)
-    marca = models.CharField(max_length=100)
-    modelo = models.CharField(max_length=100,default='Ningún modelo')
-    codigo = models.CharField(max_length=50, unique=True)
+    marca = models.CharField(max_length=255)
+    modelo = models.CharField(max_length=255, default='Ningún modelo')
+    codigo = models.CharField(max_length=255)
+    ubicacion = models.CharField(max_length=255, default='N/A')
+    utilidad = models.CharField(max_length=255, default='N/A')
 
     def __str__(self):
-        return self.nombre
+        return f"{self.nombre} - {self.marca} - {self.codigo}"
+
 
 class Movimiento(models.Model):
     usuario = models.ForeignKey('Usuario', on_delete=models.CASCADE)
@@ -45,3 +49,14 @@ class Historial(models.Model):
     def __str__(self):
         return f"{self.tipo} - {self.producto} - {self.usuario}"
 
+
+class Inventario(models.Model):
+    ActivoFijo = models.CharField(max_length=255)
+    Objeto = models.CharField(max_length=255)
+    Marca = models.CharField(max_length=255)
+    Modelo = models.CharField(max_length=255)
+    Ubicacion = models.CharField(max_length=255)
+    Utilidad = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.Objeto} - {self.ActivoFijo} - {self.Marca}"
